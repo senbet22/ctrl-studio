f83c5d47e03b0139a018af019807f1a3f632e1c6
senbet22 <senbet22@users.noreply.github.com>
2025-08-19 11:51:08 +0200

    fix for play Trailer btn

--- components/buttons/PlayButton.jsx
+++ components/buttons/PlayButton.jsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { assets } from "@/assets/assets.mjs";
+import Image from "next/image";
+
 export default function PlayButton({ onPlay }) {
   return (
     <button
       onClick={onPlay}
-      className="px-6 py-3 bg-accent/40 cursor-pointer text-foreground text-lg 
+      className="flex gap-2 px-6 py-3 bg-accent/40 cursor-pointer text-foreground text-lg 
       font-semibold rounded-lg hover:bg-secondary/70 hover:scale-105 transition-colors duration-200"
     >
-      ▶ Play Trailer
+      <Image src={assets.arrow_btn} className="size-6 rotate-180" /> Play
+      Trailer
     </button>
   );
 }
