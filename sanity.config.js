ae06dbc957be1a0d78ee289ae5e5ee2847bea58b
senbet22 <senbet22@users.noreply.github.com>
2025-09-11 18:19:24 +0200

    fix

--- sanity.config.js
+++ sanity.config.js
@@ -1,28 +1,20 @@
-'use client'
+"use client";
 
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\cms\[[...tool]]\page.jsx` route
- */
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schemaTypes'
-import {structure} from './sanity/structure'
+import { apiVersion, dataset, projectId } from "./env";
+import { schema } from "./schemaTypes";
+import { structure } from "./structure";
 
 export default defineConfig({
-  basePath: '/cms',
+  basePath: "/cms",
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    structureTool({ structure }),
+    visionTool({ defaultApiVersion: apiVersion }),
   ],
-})
+});
