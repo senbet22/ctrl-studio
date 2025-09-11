cc3d92697d315b80d79f7ed85514a3e4637e9fb9
senbet22 <senbet22@users.noreply.github.com>
2025-09-11 18:23:25 +0200

    fix sanity

--- sanity.config.js
+++ sanity.config.js
@@ -4,9 +4,10 @@ import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 
-import { apiVersion, dataset, projectId } from "./env";
-import { schema } from "./schemaTypes";
-import { structure } from "./structure";
+// Correct paths
+import { apiVersion, dataset, projectId } from "./env.js";
+import { schema } from "./schemaTypes.js";
+import { structure } from "./structure.js";
 
 export default defineConfig({
   basePath: "/cms",
