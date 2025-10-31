bf9b8544aa822d21e28d244ad493a8383b94a3dc
senbet22 <senbet22@users.noreply.github.com>
2025-10-31 17:30:08 +0100

    remove -duplicate Vision section homepage.

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -2,13 +2,10 @@ import { getDictionary } from "../dictionaries";
 
 import Header from "@/components/Header";
 import About from "@/components/About";
-import Navbar from "@/components/Navbar";
 import Lore from "@/components/Lore";
 import Team from "@/components/Team";
 import Support from "@/components/Support";
 import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-import Vision from "@/components/Vision";
 import BackToTopButton from "@/components/routing/BackToTopButton";
 
 export default async function Home({ params }) {
@@ -21,7 +18,6 @@ export default async function Home({ params }) {
       <Header dict={dict} lang={lang} />
       <About dict={dict} lang={lang} />
       <Lore dict={dict} lang={lang} />
-      <Vision dict={dict} lang={lang} />
       <Team dict={dict} lang={lang} />
       <Support dict={dict} lang={lang} />
       <Contact dict={dict} lang={lang} />
